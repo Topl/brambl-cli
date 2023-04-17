d3a8fc31b44acfb5c768ad829a61c216db32ff39
Edmundo Lopez <e.lopez@topl.me>
2023-04-17 13:32:59 -0400

    Use latest version from BramblSc

--- src/main/scala/co/topl/brambl/cli/Main.scala
+++ src/main/scala/co/topl/brambl/cli/Main.scala
@@ -1,31 +1,16 @@
 package co.topl.brambl.cli
 
+import cats.data.EitherT
 import cats.data.Validated
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
 import co.topl.brambl.cli.validation.BramblCliParamsValidatorModule
+import co.topl.brambl.models.Indices
 import co.topl.brambl.wallet.WalletApi
-import scopt.OParser
-import cats.data.EitherT
-import io.circe.syntax._
 import co.topl.crypto.encryption.VaultStore.Codecs._
-import co.topl.crypto.encryption.VaultStore
-import cats.Monad
-import co.topl.crypto.signing.{KeyPair => CryptoKeyPair}
-import co.topl.crypto.signing.ExtendedEd25519
-import quivr.models.KeyPair
-import scala.util.Try
-import co.topl.crypto.signing.Ed25519
-import co.topl.crypto.generation.Bip32Indexes
-import quivr.models.SigningKey
-import com.google.protobuf.ByteString
-import quivr.models.VerificationKey
-import co.topl.crypto.encryption.kdf.Kdf
-import co.topl.crypto.encryption.cipher.Cipher
-import co.topl.crypto.encryption.kdf.SCrypt
-import co.topl.crypto.encryption.cipher.Aes
-import co.topl.crypto.encryption.Mac
+import io.circe.syntax._
+import scopt.OParser
 
 object Main extends IOApp {
 
@@ -62,125 +47,46 @@ object Main extends IOApp {
     } yield ()).value
   }
 
-  def decodeWallet[F[_]: Monad](vaultStore: VaultStore[F], password: String) = {
-    for {
-      decoded <- EitherT(
-        VaultStore.decodeCipher[F](vaultStore, password.getBytes())
-      ).leftMap(x => new Throwable("Error" + x))
-    } yield decoded
-  }
 
-  def pbKeyPairToCryotoKeyPair(
-      pbKeyPair: KeyPair
-  ): CryptoKeyPair[ExtendedEd25519.SecretKey, ExtendedEd25519.PublicKey] = {
-    CryptoKeyPair(
-      ExtendedEd25519.SecretKey(
-        pbKeyPair.sk.sk.extendedEd25519.get.leftKey.toByteArray(),
-        pbKeyPair.sk.sk.extendedEd25519.get.rightKey.toByteArray(),
-        pbKeyPair.sk.sk.extendedEd25519.get.chainCode.toByteArray()
-      ),
-      ExtendedEd25519.PublicKey(
-        Ed25519.PublicKey(
-          pbKeyPair.vk.vk.extendedEd25519.get.vk.value.toByteArray()
-        ),
-        pbKeyPair.vk.vk.extendedEd25519.get.chainCode.toByteArray()
-      )
-    )
+  def readInputFile(someInputFile: Option[String]) = {
+    someInputFile match {
+      case Some(inputFile) =>
+        dataApi.getMainKeyVaultStore(inputFile).map(_.left.map(_.getCause()))
+      case None =>
+        IO.raiseError(new Throwable("No input file (should not happen)"))
+    }
   }
 
   def loadKeysFromParam(params: BramblCliValidatedParams) = {
-    (for {
-      wallet <- EitherT(
-        params.someInputFile
-          .map(inputFile => dataApi.getMainKeyVaultStore(inputFile))
-          .getOrElse(
-            IO(Left(new Throwable("No input file (should not happen)")))
-          )
-      )
-      decoded <- decodeWallet(wallet, params.password)
-      pbKeyPair <- EitherT(IO(Try(KeyPair.parseFrom(decoded)).toEither))
-      cryptoKeyPair <- EitherT(
-        IO(Try(pbKeyPairToCryotoKeyPair(pbKeyPair)).toEither)
-      )
-    } yield cryptoKeyPair).value
-  }
-
-  def deriveChildKeyFromKeyPair[F[_]: Monad](
-      kp: CryptoKeyPair[ExtendedEd25519.SecretKey, ExtendedEd25519.PublicKey],
-      x: Long,
-      y: Long,
-      z: Long
-  )(implicit instance: ExtendedEd25519) = {
-    import cats.implicits._
     for {
-      xCoordinate <- Monad[F].pure(Bip32Indexes.HardenedIndex(x))
-      yCoordinate <- Monad[F].pure(Bip32Indexes.SoftIndex(y))
-      zCoordinate <- Monad[F].pure(Bip32Indexes.SoftIndex(z))
-    } yield instance.deriveKeyPairFromChildPath(
-      kp.signingKey,
-      List(xCoordinate, yCoordinate, zCoordinate)
-    )
+      walletEither <- readInputFile(params.someInputFile)
+      wallet <- IO.fromEither(walletEither)
+      keyPair <- walletApi
+        .extractMainKey(wallet, params.password.getBytes())
+        .map(
+          _.left.map(_ => new Throwable("No input file (should not happen)"))
+        )
+    } yield keyPair
   }
-
-  def cryptoToPbKeyPair(
-      keyPair: CryptoKeyPair[
-        ExtendedEd25519.SecretKey,
-        ExtendedEd25519.PublicKey
-      ]
-  ): KeyPair = {
-    val skCrypto = keyPair.signingKey
-    val sk = SigningKey.ExtendedEd25519Sk(
-      ByteString.copyFrom(skCrypto.leftKey),
-      ByteString.copyFrom(skCrypto.rightKey),
-      ByteString.copyFrom(skCrypto.chainCode)
-    )
-    val vkCrypto = keyPair.verificationKey
-    val vk = VerificationKey.ExtendedEd25519Vk(
-      VerificationKey.Ed25519Vk(ByteString.copyFrom(vkCrypto.vk.bytes)),
-      ByteString.copyFrom(vkCrypto.chainCode)
-    )
-    KeyPair(
-      VerificationKey(VerificationKey.Vk.ExtendedEd25519(vk)),
-      SigningKey(SigningKey.Sk.ExtendedEd25519(sk))
-    )
-  }
-
-  val kdf: Kdf[IO] = SCrypt.make[IO](SCrypt.SCryptParams(SCrypt.generateSalt))
-  val cipher: Cipher[IO] = Aes.make[IO](Aes.AesParams(Aes.generateIv))
-
-  def buildMainKeyVaultStore(
-      mainKey: Array[Byte],
-      password: Array[Byte]
-  ): IO[VaultStore[IO]] = for {
-    derivedKey <- kdf.deriveKey(password)
-    cipherText <- cipher.encrypt(mainKey, derivedKey)
-    mac = Mac.make(derivedKey, cipherText).value
-  } yield VaultStore[IO](kdf, cipher, cipherText, mac)
 
   def deriveChildKeyFromParams(
       params: BramblCliValidatedParams
-  )(implicit instance: ExtendedEd25519) = {
+  ) = {
     for {
       kpEither <- loadKeysFromParam(params)
       kp <- IO.fromEither(kpEither)
       derivedKeyPair <-
-        deriveChildKeyFromKeyPair[IO](
+        walletApi.deriveChildKeys(
           kp,
-          params.coordinates(0).toLong,
-          params.coordinates(1).toLong,
-          params.coordinates(2).toLong
+          new Indices(
+            params.coordinates(0).toInt,
+            params.coordinates(1).toInt,
+            params.coordinates(2).toInt
+          )
         )
-      _ <- IO.fromEither(ExtendedEd25519.validate(derivedKeyPair.signingKey).left.map(x => new Throwable(x.toString())))
-      pbKeyPair <- IO(cryptoToPbKeyPair(derivedKeyPair).toByteArray)
-      vaultStore <- buildMainKeyVaultStore(
-        pbKeyPair,
-        params.password.getBytes()
-      )
-      _ <- params.someOutputFile
-        .map(f => walletApi.saveWallet(vaultStore, f))
-        .getOrElse(IO.println(vaultStore.asJson.noSpaces))
     } yield derivedKeyPair
   }
+
   override def run(args: List[String]): IO[ExitCode] = {
     OParser.parse(paramParser, args, BramblCliParams()) match {
       case Some(params) =>
@@ -190,7 +96,7 @@ object Main extends IOApp {
               case (BramblCliMode.key, BramblCliSubCmd.generate) =>
                 createWalletFromParams(validateParams).map(_ => ())
               case (BramblCliMode.key, BramblCliSubCmd.derive) =>
-                deriveChildKeyFromParams(validateParams)(new ExtendedEd25519).map(_ => ())
+                deriveChildKeyFromParams(validateParams)
             }
           case Validated.Invalid(errors) =>
             IO.println("Invalid params") *> IO.println(
