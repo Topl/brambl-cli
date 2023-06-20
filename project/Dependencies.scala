24c24389c3c2dc40103dc17487db2452a3fc3237
Diadem <d.shoukralla@topl.me>
2023-06-20 12:54:06 -0400

    update dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   lazy val toplOrg = "com.github.Topl"
 
-  lazy val bramblVersion = "76e02e2"
+  lazy val bramblVersion = "fc5cb98"
   val bramblSdk = toplOrg % "BramblSc" % bramblVersion
   // val bramblCrypto = toplOrg % "crypto" % bramblVersion
   lazy val pbVersion = "e03a093"
