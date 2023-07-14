bf62887cf1e30be1fc79db533dded436f853e933
Edmundo Lopez <e.lopez@topl.me>
2023-07-14 15:03:21 -0400

    TSDK-470 Add new test for bifrost query controller

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.10.4")
 addSbtPlugin("com.github.sbt" % "sbt-ci-release" % "1.5.12")
+addSbtPlugin("org.scoverage"    % "sbt-scoverage"       % "2.0.5")
--- src/test/scala/co/topl/brambl/cli/controllers/BifrostQueryControllerSpec.scala
+++ src/test/scala/co/topl/brambl/cli/controllers/BifrostQueryControllerSpec.scala
@@ -8,6 +8,7 @@ import co.topl.node.models.BlockBody
 import munit.CatsEffectSuite
 import co.topl.brambl.cli.modules.DummyObjects
 import co.topl.brambl.cli.views.BlockDisplayOps
+import co.topl.brambl.models.TransactionId
 
 class BifrostQueryControllerSpec extends CatsEffectSuite with DummyObjects {
 
@@ -42,6 +43,77 @@ class BifrostQueryControllerSpec extends CatsEffectSuite with DummyObjects {
       .blockByHeight(1)
       .assertEquals(
         Right(BlockDisplayOps.display(blockId01, Seq(iotransaction01)))
+      )
+  }
+
+  test("blockById should return error when block is not there") {
+    val bifrostQueryController = new BifrostQueryController[IO](
+      new BaseBifrostQueryAlgebra[IO] {
+
+        override def blockById(
+            blockId: BlockId
+        ): IO[Option[(BlockId, BlockBody, Seq[IoTransaction])]] = IO(None)
+
+      }
+    )
+    bifrostQueryController
+      .blockById("A7k6tpK25N5ZvmjkYn8jN6CnP8u9aNheT9cYb7ZjS3PN")
+      .assertEquals(
+        Left("No blocks found at that block id")
+      )
+  }
+
+  test("blockById should display a block when it is there") {
+    val bifrostQueryController = new BifrostQueryController[IO](
+      new BaseBifrostQueryAlgebra[IO] {
+
+        override def blockById(
+            blockId: BlockId
+        ): IO[Option[(BlockId, BlockBody, Seq[IoTransaction])]] =
+          IO(Some((blockId01, blockBody01, Seq(iotransaction01))))
+
+      }
+    )
+    bifrostQueryController
+      .blockById("A7k6tpK25N5ZvmjkYn8jN6CnP8u9aNheT9cYb7ZjS3PN")
+      .assertEquals(
+        Right(BlockDisplayOps.display(blockId01, Seq(iotransaction01)))
+      )
+  }
+
+  test("fetchTransaction should return error when transaction is not there") {
+    val bifrostQueryController = new BifrostQueryController[IO](
+      new BaseBifrostQueryAlgebra[IO] {
+
+        override def fetchTransaction(
+            txId: TransactionId
+        ): IO[Option[IoTransaction]] = IO(None)
+
+      }
+    )
+    bifrostQueryController
+      .fetchTransaction("A7k6tpK25N5ZvmjkYn8jN6CnP8u9aNheT9cYb7ZjS3PN")
+      .assertEquals(
+        Left(
+          "No transaction found with id A7k6tpK25N5ZvmjkYn8jN6CnP8u9aNheT9cYb7ZjS3PN"
+        )
+      )
+  }
+
+  test("fetchTransaction should display a transaction when it is there") {
+    val bifrostQueryController = new BifrostQueryController[IO](
+      new BaseBifrostQueryAlgebra[IO] {
+
+        override def fetchTransaction(
+            txId: TransactionId
+        ): IO[Option[IoTransaction]] = IO(Some(iotransaction01))
+
+      }
+    )
+    bifrostQueryController
+      .fetchTransaction("A7k6tpK25N5ZvmjkYn8jN6CnP8u9aNheT9cYb7ZjS3PN")
+      .assertEquals(
+        Right(BlockDisplayOps.display(iotransaction01))
       )
   }
 
