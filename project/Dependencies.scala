f3ec62b5ca4f3bebf4a55f05481ee5eb9cf147e4
Diadem <d.shoukralla@topl.me>
2023-11-16 11:05:25 -0500

    Updated BramblSc Dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   lazy val toplOrg = "co.topl"
 
-  lazy val bramblVersion = "2.0.0-alpha7+14-f82c71ef-SNAPSHOT"
+  lazy val bramblVersion = "2.0.0-alpha7+12-a55f9d75-SNAPSHOT"
   val bramblSdk = toplOrg %% "brambl-sdk" % bramblVersion 
 
   val monocleCore = "dev.optics" %% "monocle-core" % "3.2.0"
