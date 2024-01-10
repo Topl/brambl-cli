4cacae091586dc12476d5941daea2f33ddfd4f25
Diadem <d.shoukralla@topl.me>
2024-01-10 15:46:48 -0500

    Update dependency and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased] - 2023-mm-dd (this date should be changed on release)
 
+## [v2.0.0-beta3] - 2024-01-10
+
 ### Changed
 
 - Various bug fixes and improvements
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   lazy val toplOrg = "co.topl"
 
-  lazy val bramblVersion = "2.0.0-beta1+5-03af7b4e-SNAPSHOT"
+  lazy val bramblVersion = "2.0.0-beta2"
   val bramblSdk = toplOrg %% "brambl-sdk" % bramblVersion 
   val circeVersion = "0.15.0-M1"
 
