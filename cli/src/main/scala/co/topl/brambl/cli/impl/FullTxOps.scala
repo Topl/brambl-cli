aea8794713d045267ab454a4b8531706a78b79f1
Edmundo Lopez <e.lopez@topl.me>
2024-04-08 15:34:56 -0400

    TSDK-776 Improve code readability

--- cli/src/main/scala/co/topl/brambl/cli/impl/FullTxOps.scala
+++ cli/src/main/scala/co/topl/brambl/cli/impl/FullTxOps.scala
@@ -21,6 +21,60 @@ object FullTxOps
     with SimpleTransactionModeModule
     with TxModeModule {
 
+  private def selectToken(token: String) = 
+    if (token == "LVL")
+            TokenType.lvl
+          else if (token.startsWith(":"))
+            TokenType.series
+          else if (token.endsWith(":"))
+            TokenType.group
+          else
+            TokenType.asset
+            
+  private def selectGroupId(token: String): Option[GroupId] =
+          if (token.endsWith(":"))
+            Some(
+              GroupId(
+                ByteString.copyFrom(
+                  Encoding.decodeFromHex(token.dropRight(1)).toOption.get
+                )
+              )
+            )
+          else if (token.startsWith(":"))
+            None
+          else if (token == "LVL")
+            None
+          else
+            Some(
+              GroupId(
+                ByteString.copyFrom(
+                  Encoding.decodeFromHex(token.split(":").head).toOption.get
+                )
+              )
+            )
+
+      def selectSeriesId(token: String): Option[SeriesId] =
+          if (token.startsWith(":"))
+            Some(
+              SeriesId(
+                ByteString.copyFrom(
+                  Encoding.decodeFromHex(token.drop(1)).toOption.get
+                )
+              )
+            )
+          else if (token.endsWith(":"))
+            None
+          else if (token == "LVL")
+            None
+          else
+            Some(
+              SeriesId(
+                ByteString.copyFrom(
+                  Encoding.decodeFromHex(token.split(":").last).toOption.get
+                )
+              )
+            )
+
   def sendFunds(
       networkId: NetworkIdentifiers,
       password: String,
@@ -89,54 +143,9 @@ object FullTxOps
           amount,
           fee,
           txFile,
-          if (token == "LVL")
-            TokenType.lvl
-          else if (token.startsWith(":"))
-            TokenType.series
-          else if (token.endsWith(":"))
-            TokenType.group
-          else
-            TokenType.asset,
-          if (token.endsWith(":"))
-            Some(
-              GroupId(
-                ByteString.copyFrom(
-                  Encoding.decodeFromHex(token.dropRight(1)).toOption.get
-                )
-              )
-            )
-          else if (token.startsWith(":"))
-            None
-          else if (token == "LVL")
-            None
-          else
-            Some(
-              GroupId(
-                ByteString.copyFrom(
-                  Encoding.decodeFromHex(token.split(":").head).toOption.get
-                )
-              )
-            ),
-          if (token.startsWith(":"))
-            Some(
-              SeriesId(
-                ByteString.copyFrom(
-                  Encoding.decodeFromHex(token.drop(1)).toOption.get
-                )
-              )
-            )
-          else if (token.endsWith(":"))
-            None
-          else if (token == "LVL")
-            None
-          else
-            Some(
-              SeriesId(
-                ByteString.copyFrom(
-                  Encoding.decodeFromHex(token.split(":").last).toOption.get
-                )
-              )
-            )
+          selectToken(token),
+          selectGroupId(token),
+          selectSeriesId(token)
         )
       )
       _ <- EitherT(
